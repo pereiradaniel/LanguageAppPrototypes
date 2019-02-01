43334f5f661113343889e4360dc6dbc5c0f4a1a0
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-02-01 10:36:38 -0500

    Fatto sperimenti

--- Javascript/Noun.js
+++ Javascript/Noun.js
@@ -1,22 +1,7 @@
 // Funzione di classe Noun.
-
 class Noun {
   constructor(name) { this.name = name; }
-  //   englishName, englishSingular, englishPlural, englishDefinition,
-  //   sicilianName, sicilianSingular, sicilianPlural, sicilianDefinition,
-  //   nounGender, isIrregular) {
-  //   this.englishName = englishName;
-  //   this.englishSingular = englishSingular;
-  //   this.englishPlural = englishPlural;
-  //   this.englishDefinition = englishDefinition;
-  //   this.sicilianName = sicilianName;
-  //   this.sicilianSingular = sicilianSingular;
-  //   this.sicilianPlural = sicilianPlural;
-  //   this.sicilianDefinition = sicilianDefinition;
-  // }
-  test() {
-    return "TEST" + this.name;
-  }
+  test() { return "TEST" + this.name; }
 }
 
 module.exports = Noun
---
+++ Javascript/dictionaryStructureLayout.js
@@ -0,0 +1,49 @@
+const dictionary = [
+  word1: {
+    type: "noun",
+    english: {
+      englishName: "name",
+      englishDescription: "description"
+    },
+    sicilian: {
+      sicilianName: "name",
+      sicilianDescription: "descrizione"
+    }
+  },
+
+  word2: {
+    type: "noun",
+    english: {
+      englishName: "name",
+      englishDescription: "description"
+    },
+    sicilian: {
+      sicilianName: "name",
+      sicilianDescription: "descrizione"
+    }
+  }
+];
+
+// Noun Object
+// const createNewNoun = function(noun) {
+//   var object = {};
+//   object = noun;
+//   return object;
+// }
+
+// console.log(dictionary);
+
+
+
+// var nounObj = {
+//   english: {
+//     englishName: "name",
+//     englishDescription: "description"
+//   },
+//   sicilian: {
+//     sicilianName: "nome",
+//     sicilianDescription: "descrizione"
+//   }
+// }
+
+
