4d2da07e9673c44451d4e1b3d9dcdfc3c3888093
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-02-01 01:05:04 -0500

    Initial commit

---
+++ Javascript/test.js
@@ -0,0 +1,18 @@
+// Carica le classi di parole:
+const Noun = require('./Noun.js');
+
+// Carica i moduli di test:
+const { isTypeCorrect, describeTest, verifyExistence } = require('./tests.js');
+
+
+// TESTS
+
+// Noun class:
+  describeTest("Noun class should exist and be a function:");
+  var result = isTypeCorrect(Noun, "function");
+  console.log('Result is:  ' + result);
+
+  describeTest("An instance of a Noun can be created:");
+  const word = new Noun("NounName");
+  var result = verifyExistence(word);
+  console.log('Result is:  ' + result);
