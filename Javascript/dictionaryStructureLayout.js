72daa0bd0e6904fcb8b0ee24e63e3b4431394eac
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-02-04 10:42:48 -0500

    Aggiunti pseudo code a dictionaryStructureLayout,js

--- Javascript/dictionaryStructureLayout.js
+++ Javascript/dictionaryStructureLayout.js
@@ -24,6 +24,37 @@ const dictionary = [
   }
 ];
 
+// Access a word's English name property:
+//  dictionary.word1.english.englishName;
+
+// A program could load a word to test the user on:
+// $  let testWord = dictionary.word1;
+//  console.log('Translate this word: ' + testWord.english.englishName);
+
+// A program could generate a question for the user:
+// $  let testAnswer = translateThisWord(testWord, 'english');
+// const translateThisWord(word, testLanguage) {
+//   if (testLanguage === 'english') {
+//     return word.sicilian.sicilianName;
+//   } else {
+//     return word.english.englishName;
+//   }
+// }
+
+// A program could check user input and compare it with the correct answer:
+//  $  isUserCorrect(testWord, testAnswer, userInput);
+//  const checkAnswer = function(word, correctAnswer, userResponse) {
+//    if (userResponse === correctAnswer) {
+//      return true;
+//    } else {
+//      return false;
+//    }
+//  }
+
+
+
+
+
 // Noun Object
 // const createNewNoun = function(noun) {
 //   var object = {};
